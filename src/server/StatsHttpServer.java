42103e7eb064e47d5345011a0b4dca045c2f0607
AlbjonTahirsylaj <albjon.tahirsylaj@student.uni-pr.edu>
2026-04-18 16:52:17 +0200

    Shtimi i endpoint-it /stats dhe implementimi i handler-it dhe validimi i metodes

--- src/server/StatsHttpServer.java
+++ src/server/StatsHttpServer.java
@@ -23,3 +23,18 @@ public void start() {
         System.out.println("Gabim gjate nisjes se HTTP serverit: " + e.getMessage());
     }
 }
+httpServer.createContext("/stats", this::handleStatsRequest);
+
+System.out.println("Open: http://localhost:" + port + "/stats");
+private void handleStatsRequest(HttpExchange exchange) throws IOException {
+    String method = exchange.getRequestMethod();
+
+    if (!method.equalsIgnoreCase("GET")) {
+        String response = "Method Not Allowed";
+        exchange.sendResponseHeaders(405, response.getBytes().length);
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(response.getBytes());
+        }
+        return;
+    }
+}
