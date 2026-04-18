4180401f2f89394cdca41502d4200f14d0f70d9c
Orgesa Zyberaj <zyberaj.orgesab@gmail.com>
2026-04-18 20:23:49 +0200

    Krijimi fillestar i FileMenager dhe folderit server_files

--- src/server/FileManager.java
+++ src/server/FileManager.java
@@ -1,38 +1,26 @@
 package server;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class FileManager {
 
     private static final String BASE_FOLDER = "server_files";
 
-    public FileManager() throws Exception {
-        Path path = Paths.get(BASE_FOLDER);
-        if (!Files.exists(path)) {
-            Files.createDirectories(path);
+    public FileManager() {
+        createBaseFolderIfNeeded();
+    }
+
+    private void createBaseFolderIfNeeded() {
+        try {
+            Path basePath = Paths.get(BASE_FOLDER);
+            if (!Files.exists(basePath)) {
+                Files.createDirectories(basePath);
+            }
+        } catch (IOException e) {
+            System.out.println("Gabim gjate krijimit te folderit server_files: " + e.getMessage());
         }
-    }
-
-    public String listFiles() throws Exception {
-        try (Stream<Path> stream = Files.list(Paths.get(BASE_FOLDER))) {
-            return stream
-                    .map(p -> p.getFileName().toString())
-                    .collect(Collectors.joining(", "));
-        }
-    }
-
-    public String readFile(String filename) throws Exception {
-        Path path = PathUtil.safePath(filename);
-        return Files.readString(path);
-    }
-
-    public String deleteFile(String filename) throws Exception {
-        Path path = PathUtil.safePath(filename);
-        Files.delete(path);
-        return "Deleted";
     }
 }
