b8cbbc51f1ee5b6cf38be68b3d6c16bf6d59299b
Orgesa Zyberaj <zyberaj.orgesab@gmail.com>
2026-04-18 18:14:53 +0200

    Initial project setup with FileManager skeleton

--- src/server/FileManager.java
+++ src/server/FileManager.java
@@ -1,4 +1,12 @@
-package server;
-
 public class FileManager {
+
+    private CommandHandler commandHandler;
+
+    public FileManager() {
+        commandHandler = new CommandHandler();
+    }
+
+    public String handleCommand(String message) {
+        return commandHandler.process(message);
+    }
 }
