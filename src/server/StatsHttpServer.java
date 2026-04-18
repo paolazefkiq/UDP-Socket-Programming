ba62c8a67aa0276699d9633138a88bfb649cf0e2
AlbjonTahirsylaj <albjon.tahirsylaj@student.uni-pr.edu>
2026-04-18 16:01:29 +0200

    Krijimi i klasës StatsHttpServer, deklarimi i fushave dhe konstruktori

--- src/server/StatsHttpServer.java
+++ src/server/StatsHttpServer.java
@@ -1,4 +1,12 @@
 package server;
 
 public class StatsHttpServer {
+    private final StatsStore statsStore;
+    private final int port;
+    private HttpServer httpServer;
+
+    public StatsHttpServer(StatsStore statsStore, int port) {
+        this.statsStore = statsStore;
+        this.port = port;
+    }
 }
