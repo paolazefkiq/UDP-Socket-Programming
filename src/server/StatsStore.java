e416f917e90a73d77bbafb45e14cd5127026d716
AlbjonTahirsylaj <albjon.tahirsylaj@student.uni-pr.edu>
2026-04-18 17:24:11 +0200

    implementimi dhe gjenerimi i JSON per statistika

--- src/server/StatsStore.java
+++ src/server/StatsStore.java
@@ -33,3 +33,43 @@ public class StatsStore {
         return messages.size();
     }
 }
+public synchronized String toJson() {
+    StringBuilder sb = new StringBuilder();
+
+    sb.append("{\n");
+    sb.append("  \"activeClientCount\": ").append(getActiveClientCount()).append(",\n");
+
+    sb.append("  \"activeClients\": [\n");
+    int i = 0;
+    for (Map.Entry<String, String> entry : activeClients.entrySet()) {
+        sb.append("    { \"clientKey\": \"").append(entry.getKey())
+                .append("\", \"ip\": \"").append(entry.getValue()).append("\" }");
+        if (i < activeClients.size() - 1) {
+            sb.append(",");
+        }
+        sb.append("\n");
+        i++;
+    }
+    sb.append("  ],\n");
+
+    sb.append("  \"messageCount\": ").append(getMessageCount()).append(",\n");
+
+    sb.append("  \"messages\": [\n");
+    for (int j = 0; j < messages.size(); j++) {
+        MessageLogEntry msg = messages.get(j);
+        sb.append("    { ");
+        sb.append("\"clientKey\": \"").append(msg.getClientKey()).append("\", ");
+        sb.append("\"message\": \"").append(msg.getMessage()).append("\", ");
+        sb.append("\"timestamp\": ").append(msg.getTimestamp());
+        sb.append(" }");
+
+        if (j < messages.size() - 1) {
+            sb.append(",");
+        }
+        sb.append("\n");
+    }
+    sb.append("  ]\n");
+    sb.append("}");
+
+    return sb.toString();
+}
