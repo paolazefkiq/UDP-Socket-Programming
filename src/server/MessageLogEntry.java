2b880b2a4039db067184f0ddf04141f4dcb6a1f7
erijonelshani <erijon.elshani@student.uni-pr.edu>
2026-04-18 15:18:16 +0200

    Add MessageLogEntry class with constructor

--- src/server/MessageLogEntry.java
+++ src/server/MessageLogEntry.java
@@ -1,4 +1,13 @@
 package server;
 
 public class MessageLogEntry {
+    private final String clientKey;
+    private final String message;
+    private final long timestamp;
+
+    public MessageLogEntry(String clientKey, String message, long timestamp) {
+        this.clientKey = clientKey;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
 }
