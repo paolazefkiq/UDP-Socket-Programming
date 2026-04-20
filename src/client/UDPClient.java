fc1fb79ca5979a331a4b4c7cbf983262af9785ab
Pranvera08 <pranvera.gashi18@student.uni-pr.edu>
2026-04-20 14:44:19 +0200

    Ndryshimi i ip adreses

--- src/client/UDPClient.java
+++ src/client/UDPClient.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 public class UDPClient {
 
-    private static final String SERVER_IP = "172.16.99.4"; // nese serveri eshte ne pajisje tjeter, ndryshoje me IP reale
+    private static final String SERVER_IP = "192.168.100.95"; // nese serveri eshte ne pajisje tjeter, ndryshoje me IP reale
     private static final int SERVER_PORT = 5051;
     private static final int BUFFER_SIZE = 2048;
 
